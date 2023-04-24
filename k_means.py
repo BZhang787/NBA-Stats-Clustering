ed285a8b11dcf16c018569a5e3b709e65d10bb4c
Brandon Zhang <brandonzhang0223@gmail.com>
2023-04-23 22:04:17 -0400

    updated code to show PCA variance

--- k_means.py
+++ k_means.py
@@ -6,34 +6,30 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
 import mplcursors
 
-# Load the data from a CSV file and exclude specific columns
 data = pd.read_csv('shooting_stats_cleaned.csv', usecols=['Player', 'G', 'MP', 'FG%', 'Dist.', '%2PA', '0-3A', '3-10A', '10-16A', '16-3PA', '%3PA', '%2PM', '0-3M', '3-10M', '10-16M', '16-3PM', '%3PM', '%2PAst\'d', '%3PAst\'d', '%DunksA', 'DunkM', '%C3A', 'C3%'])
 
-# Normalize specific columns using StandardScaler
 scaler = StandardScaler()
 data[['G', 'MP', 'Dist.', 'DunkM']] = scaler.fit_transform(data[['G', 'MP', 'Dist.', 'DunkM']])
 
-# Apply KMeans clustering to the data
-kmeans = KMeans(n_clusters=7)
+kmeans = KMeans(n_clusters=6)
 labels = kmeans.fit_predict(data.drop('Player', axis=1))
 
-# Calculate the silhouette score
 silhouette_avg = silhouette_score(data.drop('Player', axis=1), labels)
 print('Silhouette Score:', silhouette_avg)
 
-# Calculate the sum of squared distances
 ssd = kmeans.inertia_
 print('Sum of Squared Distance:', ssd)
 
-# Apply PCA to reduce the dimensions of the data to 2 dimensions
-pca = PCA(n_components=3)
+pca = PCA(n_components=2)
 data_reduced = pca.fit_transform(data.drop('Player', axis=1))
 
-# Plot the data points with their cluster labels
-fig, ax = plt.subplots()
-scatter = ax.scatter(data_reduced[:, 0], data_reduced[:, 1], data_reduced[:, 2], c=labels)
+variance_explained = pca.explained_variance_ratio_
 
-# Add annotations to the scatter plot using mplcursors
+print('Total variance explained by the first two components:', sum(variance_explained[:3]))
+
+fig, ax = plt.subplots()
+scatter = ax.scatter(data_reduced[:, 0], data_reduced[:, 1], c=labels)
+
 mplcursors.cursor(hover=True).connect(
     "add", lambda sel: sel.annotation.set_text(data.loc[sel.target.index]['Player']))
 
